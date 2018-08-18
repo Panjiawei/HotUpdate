cfd139de951c634abbd1d178b4aeb9abb0aeaf2a
潘嘉伟 <1042405630@qq.com>
2018-08-18 13:25:54 +0800

    热更新完成版

---
+++ app/src/main/java/com/fren/tset/hotupdate/app/MyApplication.java
@@ -0,0 +1,35 @@
+package com.fren.tset.hotupdate.app;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
+import com.tencent.bugly.Bugly;
+import com.tencent.bugly.beta.Beta;
+
+/**
+ * Created by Administrator on 2018/8/17.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // 这里实现SDK初始化，appId替换成你的在Bugly平台申请的appId
+        // 调试时，将第三个参数改为true
+        Bugly.init(this, "7bf5b1db80", true);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        // you must install multiDex whatever tinker is installed!
+        MultiDex.install(base);
+
+
+        // 安装tinker
+        Beta.installTinker();
+    }
+
+}
